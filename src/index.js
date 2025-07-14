084a8eb33a48b4d177e6cf35a8b61477c15a0e02
Anshul Ojha <anshulojha091@gmail.com>
2025-07-14 14:25:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",  
+    origin: "nebulo-six.vercel.app",  
     credentials: true,
   })
 );
